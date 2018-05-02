4363e0ebfefdfd0e453400f269e6d6befcc3af71
Priyadharshan Ravi <32198696+PriyadharshanRavi@users.noreply.github.com>
2018-05-02 18:00:47 +0530

    Update Break_time.py

--- Break_time.py
+++ Break_time.py
@@ -6,7 +6,7 @@ total_break = 3
 break_count = 0
 print("this program started on" +time.ctime())
 while(break_count < total_break):
-    time.sleep(1000)
+    time.sleep(10)
     print("break time")
     webbrowser.open("https://youtu.be/FzLpP8VBC6E")
     break_count = break_count + 1
